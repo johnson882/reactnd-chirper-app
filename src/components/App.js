c7b4ea26e201a11ef737656295e58203fdbaa8bd
Ian Johnson <ianjohnson@Mac-Book-Pro.local>
2018-11-10 16:10:19 -0800

    almost finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
-
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
@@ -9,10 +10,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+      <LoadingBar />
+      {this.props.loading === true
+        ? null
+        : <Dashboard />}
       </div>
     )
   }
 }
 
-export default connect()(App) 
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
